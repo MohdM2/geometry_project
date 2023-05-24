1f7410c81c81b965b29ee3b41648bef7dceaf2a0
MohdM2 <mohammedmahmoudm.2002@gmail.com>
2023-05-24 07:26:11 +0300

    initial commit

---
+++ src/Line.java
@@ -0,0 +1,13 @@
+public class Line {
+    private final Point start, end;
+
+    public Line(Point start, Point end) {
+        this.start = start;
+        this.end = end;
+    }
+    //getters and length function should be implemented
+
+    public double length() {
+        return 0;
+    }
+}
---
+++ src/Point.java
@@ -0,0 +1,11 @@
+public class Point {
+    private final double x, y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    //getter methods should be implemented
+
+
+}
